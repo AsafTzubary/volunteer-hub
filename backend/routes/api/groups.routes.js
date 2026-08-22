3400b964dbff48b393f0396ab9189a57b0c78dfd
taltaub <taltaub22@gmail.com>
2026-08-22 12:06:02 +0300

    add join and leave routes

--- backend/routes/api/groups.routes.js
+++ backend/routes/api/groups.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { requireAuth } = require('../../middlewares/auth');
-const { getGroupDetails, getManagedGroup, createGroup } = require('../../controllers/groups.controller');
+const { getGroupDetails, getManagedGroup, createGroup, joinGroup, leaveGroup } = require('../../controllers/groups.controller');
 
 const router = express.Router();
 
 router.get('/mine', requireAuth, getManagedGroup);
 router.get('/:id', requireAuth, getGroupDetails);
 router.post('/', requireAuth, createGroup);
+router.post('/:id/join', requireAuth, joinGroup);
+router.post('/:id/leave', requireAuth, leaveGroup);
 
 module.exports = router;

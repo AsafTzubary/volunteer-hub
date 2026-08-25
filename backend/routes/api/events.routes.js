eee23d4ff3f4c89c57a3ff9e049e5e91c3386051
taltaub <taltaub22@gmail.com>
2026-08-25 21:22:58 +0300

    add upcoming events api

--- backend/routes/api/events.routes.js
+++ backend/routes/api/events.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { requireAuth } = require('../../middlewares/auth');
-const { listGroupEvents, createEvent } = require('../../controllers/events.controller');
+const { listGroupEvents, createEvent, listUpcomingEvents, listAllUpcomingEvents } = require('../../controllers/events.controller');
 
 const router = express.Router();
 
+router.get('/upcoming', requireAuth, listUpcomingEvents);
+router.get('/all', requireAuth, listAllUpcomingEvents);
 router.get('/', requireAuth, listGroupEvents);
 router.post('/', requireAuth, createEvent);
 

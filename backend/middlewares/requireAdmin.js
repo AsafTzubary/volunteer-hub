c0323c6070fcc854b61d188d2fd10592096caff5
taltaub <taltaub22@gmail.com>
2026-08-29 15:36:06 +0300

    add requireAdmin middleware

---
+++ backend/middlewares/requireAdmin.js
@@ -0,0 +1,14 @@
+const User = require('../models/User');
+
+async function requireAdmin(req, res, next) {
+  if (!req.session.username) {
+    return res.status(401).json({ error: 'Not logged in.' });
+  }
+  const user = await User.findOne({ username: req.session.username });
+  if (!user || user.role !== 'admin') {
+    return res.status(403).json({ error: 'Forbidden.' });
+  }
+  next();
+}
+
+module.exports = { requireAdmin };

b6e067ecc3b1e65d5388dc42768dff297bad7596
taltaub <taltaub22@gmail.com>
2026-08-31 21:14:03 +0300

    created shared navbar component

---
+++ public/js/navbar.js
@@ -0,0 +1,10 @@
+document.querySelector('nav').innerHTML = `
+  <div class="container">
+    <a class="navbar-brand" href="/dashboard.html">Volunteer Hub</a>
+    <div class="d-flex gap-2 align-items-center">
+      <a id="admin-panel-btn" href="/admin.html" class="btn btn-outline-warning btn-sm d-none">Admin Panel</a>
+      <a id="my-profile-link" href="#" class="btn btn-outline-light btn-sm">My Profile</a>
+      <button id="logout-btn" class="btn btn-outline-light btn-sm">Log Out</button>
+    </div>
+  </div>
+`;
